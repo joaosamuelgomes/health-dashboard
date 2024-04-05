2ee5a0e14f3a4c26ada19d503f6c08cef59302f2
Samuel <133022102+1leumas@users.noreply.github.com>
2024-04-05 06:11:15 -0300

    fix: Corrigir tratamento de erros de autenticacao

--- API/src/controller/testConnectionController.js
+++ API/src/controller/testConnectionController.js
@@ -2,11 +2,10 @@ const pool = require("../config/dbConfig");
 
 async function testConnectionController(req, res) {
   try {
-    pool.connect().then(() => {
-      let message = "Connected to database";
-      console.log(message);
-      res.json({ status: "success", message });
-    });
+    await pool.connect();
+    let message = "Connected to database";
+    console.log(message);
+    res.json({ status: "success", message });
   } catch (error) {
     console.error("Error: ", error);
     res.status(500).json({ status: "error", message: error.message });
