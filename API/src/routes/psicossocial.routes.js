6a73e0c62877e5cf5ece1df79ae5574885ac58ea
Samuel <samuelschirmer2002@gmail.com>
2024-04-06 09:13:52 -0300

    docs: endpoints documentation

--- API/src/routes/psicossocial.routes.js
+++ API/src/routes/psicossocial.routes.js
@@ -4,6 +4,45 @@ const {
   psicossocialController,
 } = require("../controller/psicossocialController");
 
+/**
+  @route   GET api/psicossocial
+  @desc    Returns psicossocial data based on specified parameters.
+  @access  Public
+  @produces application/json
+
+  @params
+    - name: time_range
+      type: string
+      required: false
+      description: Time range of the data (format: YYYYMM-YYYYMM).
+      example: "202206-202304"
+    - name: age
+      type: string
+      required: false
+      description: Age range of the data.
+      example: "0-50"
+    - name: cid
+      type: string
+      required: false
+      description: CID of the data.
+      example: "F20"
+    - name: sex
+      type: string
+      required: false
+      description: Sex of the patient.
+      example: "M" or "F"
+
+  @responses
+    200: 
+      status - string - Status of the response.
+      data - array - Array of psicossocial data entries.
+        example: [{...}]
+    500:
+      status - string - Status of the response in case of an error.
+      message - string - Message describing the error.
+        example: "Internal Server Error"
+*/
+
 router.get("/psicossocial", (req, res) => psicossocialController(req, res));
 
 module.exports = router;
--- API/src/routes/testConnection.routes.js
+++ API/src/routes/testConnection.routes.js
@@ -4,6 +4,23 @@ const {
   testConnectionController,
 } = require("../controller/testConnectionController");
 
+/**
+  @route   GET api/testConnection
+  @desc    Returns the status of the connection to the database
+  @access  Public
+  @produces application/json
+
+  @responses
+    200:
+      status - string - Status of the response.
+      message - string - Message of the response.
+        example: "Connected to database"
+    500:
+      status - string - Status of the response in case of an error.
+      message - string - Message describing the error.
+        example: "Internal Server Error"
+*/
+
 router.get("/testConnection", testConnectionController);
 
 module.exports = router;
