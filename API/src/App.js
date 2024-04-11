0db882597b9286c593f57f85c18ba1982a6acc1e
joaosamuelgomes <joaosamuelg001@gmail.com>
2024-04-10 22:14:59 -0300

    feat: ajustes na API e puchando dados iniciais

--- API/src/App.js
+++ API/src/App.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = require("./routes");
+const cors = require('cors');
 
 const app = express();
+
+// Configuração do CORS para permitir solicitações do localhost:5173
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
+
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,106 @@
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+import React from 'react';
 
 function App() {
+  const [dados, setDados] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterSex, setFilterSex] = useState('');
+  const [maleCount, setMaleCount] = useState(0);
+  const [femaleCount, setFemaleCount] = useState(0);
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/psicossocial');
+      if (response.data) {
+        setDados(response.data.data);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (filterSex === '') {
+      setFilteredData(dados);
+    } else {
+      const filtered = dados.filter(item => item && item.sexopac === filterSex);
+      setFilteredData(filtered);
+    }
+  }, [filterSex, dados]);
+
+  useEffect(() => {
+    // Calcular a quantidade de dados para cada sexo
+    const maleData = dados.filter(item => item && item.sexopac === 'M');
+    const femaleData = dados.filter(item => item && item.sexopac === 'F');
+    setMaleCount(maleData.length);
+    setFemaleCount(femaleData.length);
+  }, [dados]);
+
+  const labels = ['Sexo'];
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Atendimentos psicológicos totais',
+      },
+    },
+  };
+
+  const datasets = [
+    {
+      label: 'Feminino',
+      data: [femaleCount],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Masculino',
+      data: [maleCount],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ];
+
+  const data = {
+    labels,
+    datasets
+  }
 
   return (
-    <>
-      <div>
-        <p> Starting Project!</p>
-      </div>
-    </>
-  )
+    <div>
+      <Bar options={options} data={data} />
+    </div>
+  );
 }
 
-export default App
+export default App;
