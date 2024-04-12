c6a8a34443808de9d5ea1010f78aa28e026cae99
joaosamuelgomes <joaosamuelg001@gmail.com>
2024-04-11 23:58:31 -0300

    feat: criando header, configurando tailwind e prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,93 @@
-import "./App.css";
 import { useState, useEffect } from "react";
 import { fetchData } from "./services/apiServices";
+import { Bar } from "react-chartjs-2";
 
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-
-import { Bar } from "react-chartjs-2";
-import React from "react";
-
-function App() {
-  const [dados, setDados] = useState([]);
-  //const [filteredData, setFilteredData] = useState([]);
-  //const [filterSex, setFilterSex] = useState("");
-  const [maleCount, setMaleCount] = useState(0);
-  const [femaleCount, setFemaleCount] = useState(0);
-  
-  // -- query params -- //
-  const [time_range, setTime_range] = useState("202402-202402");
-  const [age, setAge] = useState("");
-  const [cid, setCid] = useState("");
-  const [sex, setSex] = useState("");
-
-  ChartJS.register(
+    Chart as ChartJS,
     CategoryScale,
     LinearScale,
     BarElement,
     Title,
     Tooltip,
-    Legend
-  );
+    Legend,
+} from "chart.js";
+import Header from "./pages/Header";
 
-  // fetch data
-  useEffect(() => {
-    fetchData(time_range, age, cid, sex).then((data) => {
-      //console.log(data);
-      setDados(data.data);
-    });
-  }, [time_range, age, sex, cid]);
+function App() {
+    const [dados, setDados] = useState([]);
+    const [maleCount, setMaleCount] = useState(0);
+    const [femaleCount, setFemaleCount] = useState(0);
 
-  // useEffect(() => {
-  //   if (filterSex === "") {
-  //     setFilteredData(dados);
-  //   } else {
-  //     const filtered = dados.filter(
-  //       (item) => item && item.sexopac === filterSex
-  //     );
-  //     setFilteredData(filtered);
-  //   }
-  // }, [filterSex, dados]);
+    // -- query params -- //
+    const [time_range, setTime_range] = useState("202311-202402");
+    const [age, setAge] = useState("");
+    const [cid, setCid] = useState("");
+    const [sex, setSex] = useState("");
 
-  useEffect(() => {
-    // Calcular a quantidade de dados para cada sexo
-    const maleData = dados.filter((item) => item && item.sexopac === "M");
-    const femaleData = dados.filter((item) => item && item.sexopac === "F");
-    setMaleCount(maleData.length);
-    setFemaleCount(femaleData.length);
-  }, [dados]);
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+    );
 
-  const labels = ["Sexo"];
+    useEffect(() => {
+        fetchData(time_range, age, cid, sex).then((data) => {
+            console.log("Dados retornados:", data.data);
+            setDados(Array.isArray(data.data.data) ? data.data.data : []);
+        });
+    }, [time_range, age, sex, cid]);
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: "Atendimentos psicológicos totais",
-      },
-    },
-  };
+    useEffect(() => {
+        const maleData = dados.filter((item) => item && item.sexopac === "M");
+        const femaleData = dados.filter((item) => item && item.sexopac === "F");
+        setMaleCount(maleData.length);
+        setFemaleCount(femaleData.length);
+    }, [dados]);
 
-  const datasets = [
-    {
-      label: "Feminino",
-      data: [femaleCount],
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Masculino",
-      data: [maleCount],
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ];
+    const labels = ["Sexo"];
 
-  const data = {
-    labels,
-    datasets,
-  };
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+            },
+            title: {
+                display: true,
+                text: "Atendimentos psicológicos totais",
+            },
+        },
+    };
 
-  return (
-    <div>
-      <Bar options={options} data={data} />
-    </div>
-  );
+    const datasets = [
+        {
+            label: "Feminino",
+            data: [femaleCount],
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+        },
+        {
+            label: "Masculino",
+            data: [maleCount],
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+        },
+    ];
+
+    const data = {
+        labels,
+        datasets,
+    };
+
+    return (
+        <>
+            <Header />
+            <div className="flex w-[400px]">
+                <Bar options={options} data={data} />
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../assets/images/Logo";
+
+function Header() {
+    return (
+        <header className="w-full h-16 bg-[#3B73A7]">
+            <Logo />
+        </header>
+    );
+}
+
+export default Header;
