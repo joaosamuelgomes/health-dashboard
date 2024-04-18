32af65c13d7f114ad0bcbe7064cadae7d5e67961
Samuel <samuelschirmer2002@gmail.com>
2024-04-18 17:38:25 -0300

    refactor: convert data filtering to sql queries

--- API/src/repositories/fetchPsicossocialData.js
+++ API/src/repositories/fetchPsicossocialData.js
@@ -1,32 +1,64 @@
 const pool = require("../config/dbConfig");
 
-async function fetchPsicossocialData(sex, cid) {
-  // query inicial
-  let query = `
+async function fetchPsicossocialData(sex, cid, time_range, age) {
+    // query inicial
+    let query = `
       SELECT p.dt_atend, p.ufmun, p.idadepac, p.sexopac, s.cd_descr
       FROM psicossocial.psrs p
       JOIN psicossocial.s_cid s ON p.cidpri = s.cd_cod
+      WHERE p.ufmun = '431020'
     `;
 
-  const params = [];
+    const params = [];
 
-  // caso exista algum filtro, adiciona a query e ao params
-  if (sex) {
-    query += " AND p.sexopac = $1";
-    params.push(sex);
-  }
-  if (cid) {
-    query += ` AND s.cd_cod = $${params.length + 1}`;
-    params.push(cid);
-  }
+    /*
+     * caso exista algum filtro, adiciona a query e ao params
+     */
 
-  //   console.log("Query: ", query);
-  //   console.log("Params: ", params);
+    if (sex) {
+        query += ` AND p.sexopac = $${params.length + 1}`;
+        params.push(sex);
+    }
+    if (cid) {
+        query += ` AND s.cd_cod = $${params.length + 1}`;
+        params.push(cid);
+    }
+    if (time_range) {
+        // dividindo o intervalo de tempo
+        const [startYearMonth, endYearMonth] = time_range.split("-");
 
-  const { rows } = await pool.query(query, params);
-  return rows;
+        // pegando data de inicio e fim
+        const startDate = `${startYearMonth.slice(0, 4)}-${startYearMonth.slice(
+            4
+        )}-01`; // usando o primeiro dia do mes
+        const endDate = `${endYearMonth.slice(0, 4)}-${endYearMonth.slice(
+            4
+        )}-28`; // usando o ultimo dia do mes
+
+        query += ` AND TO_DATE(p.dt_atend, 'YYYYMM') BETWEEN $${
+            params.length + 1
+        } AND $${params.length + 2}`;
+        params.push(startDate);
+        params.push(endDate);
+    }
+    if (age) {
+        // dividindo o intervalo de idade
+        const [startAge, endAge] = age.split("-");
+
+        query += ` AND CAST(p.idadepac AS INTEGER) BETWEEN $${
+            params.length + 1
+        } AND $${params.length + 2}`;
+        params.push(startAge);
+        params.push(endAge);
+    }
+
+    //console.log("Query: ", query);
+    //console.log("Params: ", params);
+
+    const { rows } = await pool.query(query, params);
+    return rows;
 }
 
 module.exports = {
-  fetchPsicossocialData,
+    fetchPsicossocialData,
 };
--- API/src/services/psicossocialDataService.js
+++ API/src/services/psicossocialDataService.js
@@ -1,33 +1,43 @@
 const {
-  fetchPsicossocialData,
+    fetchPsicossocialData,
 } = require("../repositories/fetchPsicossocialData.js");
-const { psicossocialFilter } = require("../utils/psicossocialFilters.js");
 const { sumCid } = require("../utils/sumCid.js");
 
 async function psicossocialDataService(time_range, sex, age, cid) {
-  try {
-    // converte o sexo para maiúsculo
-    const uppercaseSex = sex ? sex.toUpperCase() : null;
-    const uppercaseCid = cid ? cid.toUpperCase() : null;
-    // busca os dados no banco de dados
-    let data = await fetchPsicossocialData(uppercaseSex, uppercaseCid);
-    //console.log(data);
-    // filtra os dados
-    if (time_range || age) {
-      data = psicossocialFilter(data, time_range, age);
+    try {
+        const uppercaseSex = sex ? sex.toUpperCase() : null;
+        const uppercaseCid = cid ? cid.toUpperCase() : null;
+
+        /*
+         * busca os dados no banco de dados
+         */
+
+        let data = await fetchPsicossocialData(
+            uppercaseSex,
+            uppercaseCid,
+            time_range,
+            age
+        );
+
+        /*
+         * soma os cids
+         */
+
+        somaCid = sumCid(data);
+        return {
+            data: data,
+            somaCids: somaCid,
+            filtros: {
+                time_range: time_range,
+                sex: uppercaseSex,
+                age: age,
+                cid: cid,
+            },
+        };
+    } catch (error) {
+        console.error("Error: ", error);
+        throw error;
     }
-    // soma os cids
-    //console.log(`data -> `,data)
-    somaCid = sumCid(data);
-    return {
-      data: data,
-      somaCids: somaCid,
-      filtros: { time_range: time_range, sex: sex, age: age, cid: cid },
-    };
-  } catch (error) {
-    console.error("Error: ", error);
-    throw error;
-  }
 }
 
 module.exports = psicossocialDataService;
