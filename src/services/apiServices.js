9acbb126397f0f4df0ac76d5283e7ac00af76175
Samuel <samuelschirmer2002@gmail.com>
2024-04-18 18:37:49 -0300

    fix: correct URL construction to handle parameters

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,24 +1,26 @@
 import { axiosApiInstance } from "./axiosConfig";
 
 export const fetchData = async (time_range, age, cid, sex) => {
-  try {
-    let url = `/psicossocial`;
-    if (time_range) {
-      url += `?time_range=${time_range}`;
+    try {
+        let url = `/psicossocial`;
+
+        if (time_range) {
+            url += `?time_range=${time_range}`;
+        }
+        if (age) {
+            url += `${time_range ? "&" : "?"}age=${age}`;
+        }
+        if (cid) {
+            url += `${time_range || age ? "&" : "?"}cid=${cid}`;
+        }
+        if (sex) {
+            url += `${time_range || age || cid ? "&" : "?"}sex=${sex}`;
+        }
+
+        const res = await axiosApiInstance.get(url);
+        console.log(`fetch url -> `, url);
+        return res.data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
     }
-    if (age) {
-      url += `&age=${age}`;
-    }
-    if (cid) {
-      url += `&cid=${cid}`;
-    }
-    if (sex) {
-      url += `&sex=${sex}`;
-    }
-    const res = await axiosApiInstance.get(url);
-    console.log(`fetch url -> `, url);
-    return res.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
 };
