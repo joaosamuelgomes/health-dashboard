c4c78668029b7b88936a899824038073f7d2c8f3
joaosamuelgomes <joaosamuelg001@gmail.com>
2024-06-16 16:17:17 -0300

    feat: adicionando funcionalidades para a tabela, criando espaço no canto inferior direito

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -2,25 +2,23 @@ import { axiosApiInstance } from "./axiosConfig";
 
 export const fetchData = async (time_range, age, cid, sex) => {
     try {
-        let url = `/psicossocial`;
+        let url = "/psicossocial";
 
-        if (time_range) {
-            url += `?time_range=${time_range}`;
-        }
-        if (age) {
-            url += `${time_range ? "&" : "?"}age=${age}`;
-        }
-        if (cid) {
-            url += `${time_range || age ? "&" : "?"}cid=${cid}`;
-        }
-        if (sex) {
-            url += `${time_range || age || cid ? "&" : "?"}sex=${sex}`;
+        const params = [];
+        if (time_range) params.push(`time_range=${time_range}`);
+        if (age) params.push(`age=${age}`);
+        if (cid) params.push(`cid=${cid}`);
+        if (sex) params.push(`sex=${sex}`);
+
+        if (params.length > 0) {
+            url += `?${params.join("&")}`;
         }
 
-        const res = await axiosApiInstance.get(url);
         console.log(`fetch url -> `, url);
+        const res = await axiosApiInstance.get(url);
         return res.data;
     } catch (error) {
         console.error("Error fetching data:", error);
+        throw error; // Rejogar o erro para ser tratado onde a função fetchData é chamada
     }
 };
