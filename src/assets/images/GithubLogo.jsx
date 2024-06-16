934150b5231e920f850725c79ae6ef592225056d
joaosamuelgomes <joaosamuelg001@gmail.com>
2024-06-16 16:15:50 -0300

    feat: Adicionando assets para o header

---
+++ src/assets/images/GithubLogo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import github from "./github.svg";
+
+function GithubLogo() {
+    return (
+        <img src={github} alt="Github" className="h-8 w-8"/>
+    );
+}
+
+export default GithubLogo;
