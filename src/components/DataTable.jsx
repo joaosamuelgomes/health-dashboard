32a4f94c833cc094e4afa7a81a04d66a438abf5c
João Samuel Gomes <55288463+joaosamuelgomes@users.noreply.github.com>
2024-06-30 17:50:24 -0300

    Revert "fix: "feat: adicionando funcionalidades para a tabela, criando espaço no canto inferior direito""
    This reverts commit 600abf9a63a4edc42e464b67f4cd80550d555900.

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Table,
     TableBody,
@@ -10,13 +10,54 @@ import {
     CircularProgress,
     Box,
     Typography,
+    TableSortLabel,
 } from "@mui/material";
 
-function DataTable({ data, isLoading }) {
+function DataTable({ data, isLoading, onCidUpdate }) {
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('');
+    const [selectedCID, setSelectedCID] = useState('');
+
+    const handleRequestSort = (property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+        // const handleDoubleClick = (cid) => {
+        //     const shortCID = cid.split(' ')[0];
+        //     setSelectedCID(shortCID);
+        // };
+
+    useEffect(() => {
+        if (selectedCID) {
+            onCidUpdate(selectedCID);
+        }
+    }, [selectedCID, onCidUpdate]);
+
+    const sortedData = data.slice().sort((a, b) => {
+        if (orderBy === 'total') {
+            const totalA = a.genderCounts.F + a.genderCounts.M;
+            const totalB = b.genderCounts.F + b.genderCounts.M;
+            return order === 'asc' ? totalA - totalB : totalB - totalA;
+        }
+        
+        if (orderBy.startsWith('genderCounts.')) {
+            const key = orderBy.split('.')[1];
+            return order === 'asc' ? a.genderCounts[key] - b.genderCounts[key] : b.genderCounts[key] - a.genderCounts[key];
+        }
+
+        if (orderBy) {
+            return order === 'asc' ? a[orderBy].localeCompare(b[orderBy]) : b[orderBy].localeCompare(a[orderBy]);
+        }
+
+        return 0;
+    });
+
     return (
         <TableContainer component={Paper}>
             {isLoading ? (
-                <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" sx={{ minHeight: '65vh' }}>
+                <Box className="flex flex-col items-center justify-center min-h-[71.5vh]">
                     <CircularProgress />
                     <Typography variant="h6" mt={2}>Carregando...</Typography>
                 </Box>
@@ -24,45 +65,61 @@ function DataTable({ data, isLoading }) {
                 <>
                     <Table aria-label="simple table">
                         <TableHead>
-                            <TableRow sx={{ '& th': { fontWeight: 'bold' } }}>
-                                <TableCell align="left" width="400px">CID</TableCell>
-                                <TableCell align="center" width="200px">Feminino</TableCell>
-                                <TableCell align="center" width="200px">Masculino</TableCell>
-                                <TableCell align="center" width="200px">Total</TableCell>
+                            <TableRow className="bg-gray-100">
+                                <TableCell align="left" width="400px" className="font-bold">
+                                    <TableSortLabel
+                                        active={orderBy === 'name'}
+                                        direction={orderBy === 'name' ? order : 'asc'}
+                                        onClick={() => handleRequestSort('name')}
+                                    >
+                                        CID
+                                    </TableSortLabel>
+                                </TableCell>
+                                <TableCell align="center" width="200px" className="font-bold">
+                                    <TableSortLabel
+                                        active={orderBy === 'genderCounts.F'}
+                                        direction={orderBy === 'genderCounts.F' ? order : 'asc'}
+                                        onClick={() => handleRequestSort('genderCounts.F')}
+                                    >
+                                        Feminino
+                                    </TableSortLabel>
+                                </TableCell>
+                                <TableCell align="center" width="200px" className="font-bold">
+                                    <TableSortLabel
+                                        active={orderBy === 'genderCounts.M'}
+                                        direction={orderBy === 'genderCounts.M' ? order : 'asc'}
+                                        onClick={() => handleRequestSort('genderCounts.M')}
+                                    >
+                                        Masculino
+                                    </TableSortLabel>
+                                </TableCell>
+                                <TableCell align="center" width="200px" className="font-bold">
+                                    <TableSortLabel
+                                        active={orderBy === 'total'}
+                                        direction={orderBy === 'total' ? order : 'asc'}
+                                        onClick={() => handleRequestSort('total')}
+                                    >
+                                        Total
+                                    </TableSortLabel>
+                                </TableCell>
                             </TableRow>
                         </TableHead>
                     </Table>
-                    <TableContainer
-                        sx={{
-                            maxHeight: '65vh' ,
-                            overflowY: 'auto',
-                            '&::-webkit-scrollbar': {
-                                width: '8px',
-                            },
-                            '&::-webkit-scrollbar-thumb': {
-                                backgroundColor: '#888',
-                                borderRadius: '4px',
-                            },
-                            '&::-webkit-scrollbar-thumb:hover': {
-                                backgroundColor: '#555',
-                            },
-                        }}
-                    >
+                    <TableContainer className="max-h-[65vh] overflow-y-auto custom-scrollbar">
                         <Table aria-label="simple table">
                             <TableBody>
-                                {data.map((row) => (
+                                {sortedData.map((row) => (
                                     <TableRow
                                         key={row.name}
-                                        sx={{
-                                            '&:last-child td, &:last-child th': {
-                                                border: 0,
-                                            },
-                                            '&:hover': {
-                                                backgroundColor: '#f5f5f5',
-                                            },
-                                        }}
+                                        className="hover:bg-gray-100"
                                     >
-                                        <TableCell component="th" scope="row" align="left" width="400px">
+                                        <TableCell
+                                            component="th"
+                                            scope="row"
+                                            align="left"
+                                            width="400px"
+                                            onDoubleClick={() => handleDoubleClick(row.name)}
+                                        >
                                             {row.name}
                                         </TableCell>
                                         <TableCell align="center" width="200px">
@@ -79,6 +136,11 @@ function DataTable({ data, isLoading }) {
                             </TableBody>
                         </Table>
                     </TableContainer>
+                    {sortedData.length === 0 && (
+                        <Box className="flex flex-col items-center justify-center min-h-[65vh]">
+                            <Typography variant="h6" mt={2}>Nenhum dado encontrado</Typography>
+                        </Box>
+                    )}
                 </>
             )}
         </TableContainer>
